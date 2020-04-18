f38548da66c317a6fd372bfe1ee5ff357aff18fc
Roman <roman.1e2f5p8s.over3@gmail.com>
2020-04-18 14:52:00 +0100

    log-mel feature extractor implemented

--- hparams.py
+++ hparams.py
@@ -5,16 +5,16 @@ class HParamsFromYAML:
     '''
     Loads hyperparameters from yaml file into HParamsFromYAML object
     '''
-    def __init__(self, yaml_file, model_name):
+    def __init__(self, yaml_file, param_set='default'):
         '''
         Initialisation
         Arguments:
             - yaml_file -- name of yaml file, str
-            - model_name -- name of model, str
+            - param_set -- set of parameters, str. Default value is 'default'
         '''
         self.yaml_file = yaml_file
-        self.model_name = model_name
+        self.param_set = param_set
         with open(yaml_file) as f:
-            self.model_params = YAML(typ='safe').load(f)[model_name]
+            self.model_params = YAML(typ='safe').load(f)[param_set]
         for name in self.model_params:
             setattr(self, name, self.model_params[name])
