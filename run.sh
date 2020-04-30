802195dd71a8975de3c5e9512b5df28a1e75b192
Roman <roman.1e2f5p8s.over3@gmail.com>
2020-04-30 13:04:19 +0100

    torch.load with map_location

---
+++ run.sh
@@ -0,0 +1,23 @@
+#python main.py train --model=VGGish --features=log_mel --validate --manually_verified_only --shuffle \
+    #--verbose
+#sleep 600
+#python main.py train --model=VGGish --features=mfcc --validate --manually_verified_only --shuffle \
+    #--verbose
+#sleep 600
+#python main.py train --model=VGGish --features=chroma --validate --manually_verified_only --shuffle \
+    #--verbose
+#sleep 600
+
+#python plot.py train --model=VGGish --features=log_mel --validated --manually_verified_only --verbose
+#python plot.py train --model=VGGish --features=mfcc --validated --manually_verified_only --verbose
+#python plot.py train --model=VGGish --features=chroma --validated --manually_verified_only --verbose
+
+#python main.py validation --model=VGGish --features=log_mel --epoch=15 --validated \
+    #--manually_verified_only --shuffle --verbose
+#python plot.py validation --model=VGGish --features=log_mel --epoch=15 --validated \
+    #--manually_verified_only --verbose
+
+python main.py test --model=VGGish --features=log_mel --epoch=15 --validated \
+    --manually_verified_only --verbose
+python plot.py test --model=VGGish --features=log_mel --epoch=15 --validated \
+    --manually_verified_only --verbose
